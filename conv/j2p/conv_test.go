4afa8f79fce4cbb32e8767e63d98a6abfa83b7c6
khannb <838278270@qq.com>
2024-06-17 03:55:07 +0800

    test: test sint/fix types map

--- conv/j2p/conv_test.go
+++ conv/j2p/conv_test.go
@@ -285,8 +285,8 @@ func getBasicExampleReq() *base.BasicExample {
 	req = &base.BasicExample{
 		Int32:        123,
 		Int64:        123,
-		Uint32:       123,
-		Uint64:       123,
+		Uint32:       uint32(math.MaxInt32),
+		Uint64:       uint64(math.MaxInt64),
 		Sint32:       123,
 		Sint64:       123,
 		Sfixed32:     123,
@@ -313,6 +313,102 @@ func getBasicExampleReq() *base.BasicExample {
 		ListBool:     []bool{true, false, true, false, true},
 		ListString:   []string{"a1", "b2", "c3", "d4", "e5"},
 		ListBytes:    [][]byte{{0x1, 0x2, 0x3, 0x4}, {0x5, 0x6, 0x7, 0x8}},
+		MapInt64SINT32: map[int64]int32{
+			0:             0,
+			math.MaxInt64: math.MaxInt32,
+			math.MinInt64: math.MinInt32,
+		},
+		MapInt64Sfixed32: map[int64]int32{
+			0:             0,
+			math.MaxInt64: math.MaxInt32,
+			math.MinInt64: math.MinInt32,
+		},
+		MapInt64Fixed32: map[int64]uint32{
+			math.MaxInt64: uint32(math.MaxInt32),
+			math.MinInt64: 0,
+		},
+		MapInt64Uint32: map[int64]uint32{
+			0:             0,
+			math.MaxInt64: uint32(math.MaxInt32),
+			math.MinInt64: 0,
+		},
+		MapInt64Double: map[int64]float64{
+			0:             0,
+			math.MaxInt64: math.MaxFloat64,
+			math.MinInt64: math.SmallestNonzeroFloat64,
+		},
+		MapInt64Bool: map[int64]bool{
+			0:             false,
+			math.MaxInt64: true,
+			math.MinInt64: false,
+		},
+		MapInt64String: map[int64]string{
+			0:             "0",
+			math.MaxInt64: "max",
+			math.MinInt64: "min",
+		},
+		MapInt64Bytes: map[int64][]byte{
+			0:             {0x0},
+			math.MaxInt64: {0x1, 0x2, 0x3, 0x4},
+			math.MinInt64: {0x5, 0x6, 0x7, 0x8},
+		},
+		MapInt64Float: map[int64]float32{
+			0:             0,
+			math.MaxInt64: math.MaxFloat32,
+			math.MinInt64: math.SmallestNonzeroFloat32,
+		},
+		MapInt64Int32: map[int64]int32{
+			0:             0,
+			math.MaxInt64: math.MaxInt32,
+			math.MinInt64: math.MinInt32,
+		},
+		MapstringSINT64: map[string]int64{
+			"0":   0,
+			"max": math.MaxInt64,
+			"min": math.MinInt64,
+		},
+		MapstringSfixed64: map[string]int64{
+			"0":   0,
+			"max": math.MaxInt64,
+			"min": math.MinInt64,
+		},
+		MapstringFixed64: map[string]uint64{
+			"max": uint64(math.MaxInt64),
+			"min": 0,
+		},
+		MapstringUint64: map[string]uint64{
+			"max": uint64(math.MaxInt64),
+			"min": 0,
+		},
+		MapstringDouble: map[string]float64{
+			"0":   0,
+			"max": math.MaxFloat64,
+			"min": math.SmallestNonzeroFloat64,
+		},
+		MapstringBool: map[string]bool{
+			"0":   false,
+			"max": true,
+		},
+		MapstringString: map[string]string{
+			"0":   "0",
+			"max": "max",
+			"min": "min",
+		},
+		MapstringBytes: map[string][]byte{
+			"0":   {0x0},
+			"max": {0x1, 0x2, 0x3, 0x4},
+			"min": {0x5, 0x6, 0x7, 0x8},
+		},
+		MapstringFloat: map[string]float32{
+			"0":   0,
+			"max": math.MaxFloat32,
+			"min": math.SmallestNonzeroFloat32,
+		},
+		MapstringInt64: map[string]int64{
+			"0":   0,
+			"max": math.MaxInt64,
+			"min": math.MinInt64,
+		},
 	}
 
 	return req
