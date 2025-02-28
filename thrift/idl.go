7d81d39ba60048530d72f1108c63ed46ade9aab3
Marina-Sakai <marina.sakai@bytedance.com>
2025-02-28 17:44:20 +0800

    optimize: reduce memory allocations

--- thrift/idl.go
+++ thrift/idl.go
@@ -209,7 +209,7 @@ func parseIDLContent(path, content string, includes map[string]string, isAbsIncl
 func refIncludes(tree *parser.Thrift, path string, done map[string]*parser.Thrift, includes map[string]*parser.Thrift, isAbsIncludePath bool) error {
 	done[path] = tree
 	for _, i := range tree.Includes {
-		var ps []string
+		ps := make([]string, 0, 2)
 		if isAbsIncludePath {
 			p := absPath(tree.Filename, i.Path)
 			if p != i.Path {
